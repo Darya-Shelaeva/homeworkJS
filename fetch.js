3f2514fb7b9bc60841fb10822bd3562c9e9e0da5
Darya-Shelaeva <selaevadarya03@gmail.com>
2023-08-30 14:41:45 +0300

    fetch

---
+++ fetch.js
@@ -0,0 +1,52 @@
+'use strict'
+/*
+let isLoading = false
+
+fetch('https://645f57d47da4477ba9554f96.mockapi.io/users')
+.then (data => {
+isLoading = true
+return data.json();
+
+})
+
+.then(data => {
+    isLoading = true
+    document.querySelector('.cont').innerHTML = data.map((item) => `<div>${item.name}</div>`)
+})
+
+.catch((error) => {
+    isLoading = true
+    console.log('Ошибка', error)
+})
+
+.finally(() => {
+    isLoading = false
+})*/
+
+
+/*
+const fetchData = async() => {
+
+    try {
+        const data = await fetch ('https://645f57d47da4477ba9554f96.mockapi.io/users')
+        const procData = await data.json()
+        console.log(procData)
+    } catch (error){
+        console.log (error)
+    } 
+};
+
+fetchData()*/
+
+/*
+const arr = ['h', 'o', 'l', 'a'];
+
+function printArray (array) {
+    return array.join(',')
+}
+
+console.log(printArray(arr))*/
+
+
+//let isLoading = false
+
---
+++ home_fetch.js
@@ -0,0 +1,51 @@
+'use strict'
+
+let isLoading = false
+
+fetch('https://reqres.in/api/users?per_page=12')
+.then (data => {
+    isLoading = true
+    return data.json();
+})
+.then(data => {
+    isLoading = true
+    document.querySelector('.cont2').innerHTML = data.data.map((item) => `<div>${item.last_name}</div>`)
+    console.log(data.data)
+    return data.data
+})
+.then(data => {
+    //console.log (data.data)
+    const users = data
+    const filteredUsers = users.filter((item) => {
+      if(item.last_name[0] === 'F') {
+        return true;
+      }
+    })
+  console.log(filteredUsers)
+  return users;
+})
+.then(data => {
+    isLoading = true
+    const message = 'Наша база содержит данные следующих пользователей: '
+    const usersNames = data.reduce((acc, user) => {
+      return [...acc, `${user.last_name}  ${user.first_name}`]
+    }, [])
+    const namesStr = usersNames.join(', ');
+    document.querySelector('.cont3').innerHTML = `${message} ${namesStr}`
+    return data
+})
+.then(data => {
+    isLoading = true
+    console.log(Object.keys(data[0]))
+})
+        
+.catch((error) => {
+    isLoading = true
+    console.log('Ошибка', error)
+})
+
+.finally(() => {
+    isLoading = false
+})
+
+
